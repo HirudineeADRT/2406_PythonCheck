f3d39672026be4f4e59910416fb30b3ea08ddcf3
HirudineeADRT <hirudinee@adroitlogic.com>
2019-06-24 14:48:46 +0530

    test

--- 2406_PythonCheck/pythonhiru.py
+++ 2406_PythonCheck/pythonhiru.py
@@ -3,5 +3,5 @@ from pythonsub.test import add
 def handler(event, context):
 
     print (context)
-    ///test123 123 sample
+    ///test123 123 sample edited
     return {"message": "Successfully executed",add(1,3)}
